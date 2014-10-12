78e9a15713455c3471149d5918d09b2089fc64a4
Tony Malagisi <mtb3000gt@gmail.com>
2014-10-12 14:09:56 -0400

    Track and Use Our Own Kernel
    Change-Id: Iffa0c244d7a8a2691b4dbe619abd6ee02ef01be1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,9 +23,9 @@ TARGET_BOARD_INFO_FILE := device/samsung/d710/board-info.txt
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d710/bluetooth
 
-# Kernel Config
-TARGET_KERNEL_SOURCE := kernel/samsung/d710
-TARGET_KERNEL_CONFIG := kernel_defconfig
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
+TARGET_KERNEL_CONFIG := cyanogenmod_d710_defconfig
 TARGET_GCC_VERSION_ARM := 4.10-sm
 
 # Selinux
